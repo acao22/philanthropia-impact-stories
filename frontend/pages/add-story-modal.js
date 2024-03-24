f64ea222a1b13d2d835a3ead8742be7be188220e
angiecao <caoangelina@gmail.com>
2024-03-24 12:53:21 -0400

    updated add story modal functionality & styling

--- frontend/pages/add-story-modal.js
+++ frontend/pages/add-story-modal.js
@@ -17,24 +17,41 @@ function AddStoryModal({ modalIsOpen, setModalIsOpen, newStory, setNewStory, add
     setNewStory({
       title: '',
       snippet: '',
-      imageUrl: '',
+      imageUrl: newStory.imageUrl,
       organization: '',
       donor: '',
       fullStory: ''
     });
   };
 
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setNewStory({ ...newStory, imageUrl: e.target.result });
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="title" value={newStory.title} onChange={handleInputChange} placeholder="Title" />
-        <input type="text" name="snippet" value={newStory.snippet} onChange={handleInputChange} placeholder="Snippet" />
-        <input type="text" name="imageUrl" value={newStory.imageUrl} onChange={handleInputChange} placeholder="Image URL" />
-        <input type="text" name="organization" value={newStory.organization} onChange={handleInputChange} placeholder="Organization" />
-        <input type="text" name="donor" value={newStory.donor} onChange={handleInputChange} placeholder="Donor" />
-        <textarea name="fullStory" value={newStory.fullStory} onChange={handleInputChange} placeholder="Full Story"></textarea>
-        <button type="submit">Add Story</button>
-      </form>
+    <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} className="modal-wrapper custom-modal-content">
+    <form onSubmit={handleSubmit}>
+          <input type="text" name="title" value={newStory.title} onChange={handleInputChange} placeholder="Title" />
+          <input type="text" name="snippet" value={newStory.snippet} onChange={handleInputChange} placeholder="Snippet" />
+          <div className="file-input-container">
+            <input type="file" id="imageFile" name="imageFile" onChange={handleFileChange} />
+            <label htmlFor="imageFile" className="custom-file-upload">Upload Image</label>
+          </div>
+          <input type="text" name="organization" value={newStory.organization} onChange={handleInputChange} placeholder="Organization" />
+          <input type="text" name="donor" value={newStory.donor} onChange={handleInputChange} placeholder="Donor" />
+          <textarea name="fullStory" value={newStory.fullStory} onChange={handleInputChange} placeholder="Full Story"></textarea>
+          <button className="cube-button" type="submit">
+            <span>click here!</span>
+            <span>add story</span>
+          </button>
+        </form>
     </Modal>
   );
 }
